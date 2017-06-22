d91ea55f9a0fa4cd7c93823db2e37188d5866180
Christopher Barrett <christopherbarrettza@gmail.com>
2017-06-22 18:06:47 +0200

    Testing Checkstyle fixes

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,7 +12,10 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
+  /**
+  * This method prints out a greeting using a	
+  * String passed in the argument
+  */
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
